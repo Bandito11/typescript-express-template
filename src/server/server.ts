cdae0581c9358a02d37cd6c73c23d12c6dcc1bdf
Esteban Morales <red_bandit_11@hotmail.com>
2018-06-27 13:09:18 -0400

    minor fixes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,22 +1,26 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import * as morgan from 'morgan'; //Only in dev
 import { verifyAuthentication } from './authenticate/authenticate.module';
+/////only in development environment
+import * as morgan from 'morgan';
+require('dotenv').config();
+require('http').globalAgent.maxSockets = 5;
+app.use(morgan('dev')); //Only in dev
+////////////////////////////////////////
 
 
 const app = express();
-require('dotenv').config(); //Only in dev
-require('http').globalAgent.maxSockets = 5;  //Only in dev
 // require('http').globalAgent.maxSockets = Infinity;
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('dev')); //Only in dev
 const path = require('path');
 
 // Only allow if you want to use it as an API.
 app.use(function (req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Origin", "*"); // uncomment if server is used as API
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token, Authorization');
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
   next();
@@ -44,7 +48,7 @@ apiRoutes.use('/authenticate', authenticate);
 apiRoutes.use(verifyAuthentication);
 
 // Authenticated routes
-// apiRoutes.use('/my_custom_route', myCustomRoute);
+// apiRoutes.use('/my_custom_route', myCustomRoute); An example of using a imported route.
 
 //Error Handling, always goes last. 
 app.use(function (err, req, res, next) {
