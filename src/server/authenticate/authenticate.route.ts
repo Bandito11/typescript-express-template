912492ab7fc5f2b465b7b9cc247ba80e678da96b
Esteban Morales <BanditoMac@Estebans-Mac-mini.local>
2018-08-01 01:35:21 -0400

    1.0.8

--- src/server/authenticate/authenticate.route.ts
+++ src/server/authenticate/authenticate.route.ts
@@ -1,30 +1,30 @@
 import { Router, Request, Response } from 'express';
-import { checkAuthentication} from './authenticate.module'
+import { checkIfUserExists, registerUser } from './authenticate.module'
 const router = Router();
 
 router.post('/', authenticate);
 async function authenticate(req: Request, res: Response) {
-//     const username = req.headers.username;
-//     const password = req.headers.password;
-//     const response = await checkAuthentication({ username: username, password: password });
-//     if (response.success) {
-//         res.send(response)
-//     } else {
-//         res.send(response);
-//     }
-// }
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const response = await checkIfUserExists({ username: username, password: password });
+    if (response.success) {
+        res.send(response)
+    } else {
+        res.send(response);
+    }
+}
 
-// router.post('/register', register);
+router.post('/register', register);
 
-// async function register(req: Request, res: Response){
-//     const username = req.headers.username;
-//     const password = req.headers.password;
-//     const response = await registerUser({ username: username, password: password });
-//     try {
-//         res.send(response)
-//     } catch (error) {
-//         res.send(error);
-//     }
+async function register(req: Request, res: Response) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const response = await registerUser({ username: username, password: password });
+    try {
+        res.send(response)
+    } catch (error) {
+        res.send(error);
+    }
 }
 
 module.exports = router;
