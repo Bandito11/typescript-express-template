7ae2f06e82e5dc8c14da63d2765ca00bf5df2e72
Esteban Morales <red_bandit_11@hotmail.com>
2020-06-14 19:38:46 -0400

    first commit

---
+++ src/server/authroutes.ts
@@ -0,0 +1,30 @@
+import { Router, Request, Response } from 'express';
+import { checkIfUserExists, registerUser } from './authenticate'
+const router = Router();
+
+router.post('/', authenticate);
+async function authenticate(req: Request, res: Response) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const response = await checkIfUserExists({ username: username, password: password });
+    if (response.success) {
+        res.send(response)
+    } else {
+        res.send(response);
+    }
+}
+
+router.post('/register', register);
+
+async function register(req: Request, res: Response) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const response = await registerUser({ username: username, password: password });
+    try {
+        res.send(response)
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+module.exports = router;
