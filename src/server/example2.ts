ba65532e3e34a8e37cd8e98cc72d651e9a592e92
Esteban Morales <red_bandit_11@hotmail.com>
2020-06-14 19:33:25 -0400

    first commit

---
+++ src/server/example2.ts
@@ -0,0 +1,12 @@
+
+import { Router, Request, Response, NextFunction } from 'express';
+const router = Router();
+
+router.get('/message/:message', example);
+function example(req: Request, res: Response, next: NextFunction) {
+    const message = req.params.message;
+    const echoMessage = `Echo Message from authenticated route: ${message}`;;
+    res.send(echoMessage);
+}
+
+module.exports = router;
