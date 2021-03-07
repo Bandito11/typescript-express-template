64a6135119ecf97c18d0e558a21427789d3b6ff8
Bandito11 <red_bandit_11@hotmail.com>
2021-03-07 02:07:35 -0400

    body-parser was added to nodejs so the package was uninstalled and the code revised

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import * as bodyParser from 'body-parser'
 import helmet from 'helmet';
 import { Response } from 'express'
 import dotenv from 'dotenv';
@@ -17,8 +16,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
