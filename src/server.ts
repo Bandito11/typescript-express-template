fb403fddc239b3eb880b565b5a82885492a7f2c3
Bandito11 <red_bandit_11@hotmail.com>
2021-03-08 15:58:22 -0400

    added cors package

--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,25 @@ import helmet from 'helmet';
 import { Response } from 'express'
 import dotenv from 'dotenv';
 import http from 'http';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
+if (!process.env.NODE_ENV) {
+  dotenv.config();
+} else if (process.env.NODE_ENV === 'production') {
   http.globalAgent.maxSockets = Infinity;
 } else {
   http.globalAgent.maxSockets = 5;
-  app.use(require('morgan')('dev'));
+  app.use(morgan('dev'));
 }
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
-  next()
-})
-
-app.get('/', (req, res, next) => {
-  res.send(`Hi from locahost:${PORT}!`);
-})
 
 //Error Handling, always goes last. 
 app.use((err: Error, req, res: Response, _next) => {
